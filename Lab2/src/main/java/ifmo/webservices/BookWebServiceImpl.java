f1529868f471fa46b6a00fc8dfcb7223d0fd8c67
Anastasiya96 <razumovskaja-nastja@rambler.ru>
2019-03-08 23:24:29 +0300

    Add Lab2 folder

---
+++ Lab2/src/main/java/ifmo/webservices/BookWebServiceImpl.java
@@ -0,0 +1,23 @@
+package ifmo.webservices;
+
+import java.util.List;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(name = "BookWebService", serviceName = "BookService")
+public class BookWebServiceImpl implements BookWebService {
+
+    @WebMethod(operationName = "getAllBooks")
+    public List<Book> getAllBooks() {
+        OracleSQLDAO dao = new OracleSQLDAO();
+        List<Book> books = dao.getAllBooks();
+        return books;
+    }
+
+    @WebMethod(operationName = "getBooks")
+    public List<Book> getBooks(@WebParam(name = "conditions") List<BookCondition> conditions) {
+        OracleSQLDAO dao = new OracleSQLDAO();
+        return dao.getBooksByFields(conditions);
+    }
+}
