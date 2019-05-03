7405024be0fd1db88abd1267e5dfd5e7c1e756f0
Anastasiya96 <razumovskaja-nastja@rambler.ru>
2019-05-04 02:27:55 +0300

    Remove unused

--- Lab6/src/main/java/ifmo/webservices/errors/DatabaseException.java
+++ Lab6/src/main/java/ifmo/webservices/errors/DatabaseException.java
@@ -1,8 +1,5 @@
 package ifmo.webservices.errors;
 
-import javax.xml.ws.WebFault;
-
-@WebFault(faultBean = "ifmo.webservices.errors.BookServiceFault")
 public class DatabaseException extends Exception {
     public static DatabaseException DEFAULT_INSTANCE = new
             DatabaseException("Error occurred during database operation");
--- Lab6/src/main/java/ifmo/webservices/errors/IllegalNameException.java
+++ Lab6/src/main/java/ifmo/webservices/errors/IllegalNameException.java
@@ -1,8 +1,5 @@
 package ifmo.webservices.errors;
 
-import javax.xml.ws.WebFault;
-
-@WebFault(faultBean = "ifmo.webservices.errors.BookServiceFault")
 public class IllegalNameException extends Exception {
     public static IllegalNameException DEFAULT_INSTANCE = new
             IllegalNameException("personName cannot be null or empty");
